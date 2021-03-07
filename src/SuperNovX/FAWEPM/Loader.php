2120481ce077afd6b13fed0c85aed714474995ea
FlxiBoy <65186341+FlxiBoy1313@users.noreply.github.com>
2021-03-07 20:52:29 +0100

    Update Loader.php

--- src/SuperNovX/FAWEPM/Loader.php
+++ src/SuperNovX/FAWEPM/Loader.php
@@ -12,16 +12,10 @@ class Loader extends PluginBase
     public function onEnable()
     {
         self::$instance = $this;
-        $this->getLogger()->info("§a§aFAWE-PM was enable!");
     }
 
     public static function getInstance(): self
     {
         return self::$instance;
-    }
-    
-    public function onDisble()
-    {
-        $this->getLogger()->info("§c§lFAWE-PM was disable!");
     }
 }
